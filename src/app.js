f50c7bfdeeec11a7f6ef774a200483b05d8206ea
Mario Roberto Molano <119605659+mariormolano@users.noreply.github.com>
2025-12-03 14:24:10 -0500

    feat: se agrega las rutas al app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import personaRoutes from './routes/persona.routes.js'
+import cursosRoutes from './routes/curso.routes.js'
+
+
 const app = express()
+
+app.use(express.json())
 
 app.get("/", (_, res) => {
     res.send("hola mundo")
 })
+
+app.use("/personas", personaRoutes)
+app.use("/cursos", cursosRoutes)
 
 export default app
