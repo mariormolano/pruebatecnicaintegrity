62accd5041881d5585c5a2e0ff05956dc9ed1807
Mario Roberto Molano <119605659+mariormolano@users.noreply.github.com>
2025-12-03 18:00:51 -0500

    feat: función para actualizar persona

--- src/controllers/persona.controller.js
+++ src/controllers/persona.controller.js
@@ -75,7 +75,49 @@ export const getPersonByDNI = async (req, res) => {
 }
 
 export const updatePersonById = async (req, res) =>{
+    try {
+        const {nombre, cedula, email, curso} = req.body
+        if (!nombre || !curso || !cedula) {
+            console.log("Faltan datos")
+            res.status(400).json({
+                error:"faltan datos"
+            })
+            return
+        }
+        const {id} = req.params
+        console.log("getPersons:" + id)
+        if (!id || id.length != 24) {
+            res.status(400).json({
+                error: "ID invalido"
+            })
+            return
+        }
+        const getCurso = await cursos.findOne({codigo: curso})
+        console.log(getCurso)
+        const cursoId = getCurso._id
+        console.log(nombre, cedula, email, curso, cursoId.toString())
+        if (!cursoId.toString()){
+            res.status(400).json({
+                error:"no se encuetra el curso"
+            })
+        }
 
+        const persona = await personas.updateOne({_id:id}, {$set: {
+            nombre, 
+            cedula, 
+            email, 
+            curso:cursoId
+        }})
+        res.json({
+            data: {nombre, cedula, email, curso}
+        })
+        console.log(persona)
+    } catch (error) {
+        res.status(400).json({
+            error: "no se pudo actualizar la persona"
+        })
+        console.log(error)
+    }
 }
 
 export const deletePersonById = async (req, res) =>{
