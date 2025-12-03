83ad9f978a4be10c7f32de382b64b04b5d0c108b
Mario Roberto Molano <119605659+mariormolano@users.noreply.github.com>
2025-12-03 14:22:36 -0500

    feat: se agrega la configuracion al servidor mongoDB en local

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from './src/app.js'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
@@ -7,6 +8,8 @@ const serverPort = process.env.SERVER_PORT || 3000
 
 app.listen(serverPort, () =>{
     console.log("Servidor iniciado en http://localhost:"+serverPort)
+    mongoose.connect(process.env.MONGO_URI)
+    .then(console.log("Conectado a la base de datos"))
 })
 
 export default app
