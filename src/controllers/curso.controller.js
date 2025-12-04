98ff653541d397d12daffa06a668243e23bf6c2a
Mario Roberto Molano <119605659+mariormolano@users.noreply.github.com>
2025-12-03 22:11:52 -0500

    refactor: ajuste y estandarización de las respuestas, arreglo de fallos y optimización

--- src/controllers/curso.controller.js
+++ src/controllers/curso.controller.js
@@ -1,12 +1,23 @@
 import { cursos } from "../models/curso.model.js"
 
 export const getAllCursos = async (req, res) => {
+    const listaCursos = await cursos.find()
+    const datosListaCursos = listaCursos ? listaCursos.map((curso) => {
+        const resCurso = {
+            nombre: curso.nombre,
+            codigo: curso.codigo,
+            descripcion: curso.descripcion
+        }
+        return resCurso
+    }) : {}
     try {
         res.json({
-            data: await cursos.find()
+            status: "success",
+            data: datosListaCursos
         })
     } catch (error) {
         res.json({
+            status: "failure",
             error: "error al buscar cursos"
         })
         
@@ -19,6 +30,7 @@ export const createCurso = async (req, res) => {
         if (!nombre || !codigo) {
             console.log("Faltan datos")
             res.status(400).json({
+                status: "failure",
                 error:"faltan datos"
             })
             return
@@ -26,11 +38,13 @@ export const createCurso = async (req, res) => {
         console.log(nombre, codigo, descripcion)
         const curso = new cursos({nombre, codigo, descripcion})
         await curso.save()
-        res.json({  
-            data: {nombre, codigo, descripcion}
+        res.json({ 
+            status: "success",
+            data: "Se creo el curso exitosamente"
         })
     } catch (error) {
         res.status(409).json({
+            status: "failure",
             error: error?.errmsg?.includes("E11000") ? "Ya existe este curso" : error.errmsg
         })
         console.log(error.errmsg)
